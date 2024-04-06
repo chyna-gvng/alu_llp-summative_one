16de344b5ba926193d39cc2e3de0a12bc1a0a1c4
Chyna <w.angoye@alustudent.com>
2024-04-06 12:21:24 +0300

    Updated question_four.c

--- Q4/question_four.c
+++ Q4/question_four.c
@@ -2,8 +2,15 @@
 #include <stdlib.h>
 #include <string.h>
 
+// Comparison function pointer types
+typedef int (*Comparator)(int, int);
+
+// Function pointers for ascending and descending order
+Comparator ascendingCmp;
+Comparator descendingCmp;
+
 // Function prototypes
-void bubbleSort(int arr[], int n, int (*cmp)(int, int));
+void bubbleSort(int arr[], int n, Comparator cmp);
 int ascendingOrder(int a, int b);
 int descendingOrder(int a, int b);
 void printArray(int arr[], int n);
@@ -12,16 +19,19 @@ int main() {
     int grades[] = {23, 45, 31, 36, 32, 26, 29, 30, 33, 35, 28, 37, 19, 20};
     int n = sizeof(grades) / sizeof(grades[0]);
 
+    ascendingCmp = ascendingOrder;
+    descendingCmp = descendingOrder;
+
     char orderChoice[4];
     printf("Enter order choice (asc for ascending and desc for descending): ");
     scanf("%s", orderChoice);
 
-    int (*cmp)(int, int);
+    Comparator cmp;
 
     if (strcmp(orderChoice, "asc") == 0)
-        cmp = ascendingOrder;
+        cmp = ascendingCmp;
     else if (strcmp(orderChoice, "desc") == 0)
-        cmp = descendingOrder;
+        cmp = descendingCmp;
     else {
         printf("Invalid order choice.\n");
         return 1;
@@ -36,11 +46,11 @@ int main() {
 }
 
 // Bubble sort algorithm
-void bubbleSort(int arr[], int n, int (*cmp)(int, int)) {
+void bubbleSort(int arr[], int n, Comparator cmp) {
     int i, j, temp;
     for (i = 0; i < n-1; i++) {
         for (j = 0; j < n-i-1; j++) {
-            if ((*cmp)(arr[j], arr[j+1])) {
+            if (cmp(arr[j], arr[j+1])) {
                 // Swap elements
                 temp = arr[j];
                 arr[j] = arr[j+1];
